486f6a49dd9480939f95d7f1d9070c58215b190e
DataSpot42 <stevelookingforward@gmail.com>
2023-09-12 18:12:40 +0100

    Added new databases

--- Backend/controllers/items.js
+++ Backend/controllers/items.js
@@ -1,4 +1,4 @@
-const Todo = require("../models/punkshop")
+const Punk = require("../models/punkshop")
 
 
 const createPunk = (req, res) => {
@@ -13,7 +13,7 @@ const createPunk = (req, res) => {
 
 const getPunk = async (req, res) => {
     // find all items from a mongoose Model method 
-    const items = await punk.find({})
+    const items = await Punk.find({})
     // respond with and object that has a message and the items from the DB
     res.json({
         message: "all items",
@@ -21,9 +21,17 @@ const getPunk = async (req, res) => {
     })
 }
 
+const editPunk = async (req, res) => {
+    // get id from ':id' param from the route
+    const { id } = req.params
+    // use mongoose model method findByIdAndUpdate
+    const punk = await Punk.findByIdAndUpdate(id, { text: req.body.text })
+    res.status(200).json(punk)
+}
 module.exports = {
 
     getPunk,
-    createPunk
+    createPunk,
+    editPunk
    
 }
--- Backend/models/customer.js
+++ Backend/models/customer.js
@@ -4,23 +4,23 @@ const mongoose = require('mongoose')
 // get schema from mongoose object
 const Schema = mongoose.Schema
 // what the data will look like
-const punkSchema = new Schema({
-    orderNum: {
-        type: Number,
-        required: true
-    },
+const customerSchema = new Schema({
     custID: {
         type: Number,
         required: true
     },
+    
     items: [{
         item: String,     
-        type: Number,
         quantity: Number,
         price: Number,
         required: true
         
-    }] 
+    }], 
+    total: {
+        type: Number,
+        required: true
+    }
     
 })
 
